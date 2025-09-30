73ce999bc23f12300ce6f8c2efa5165169a25d46
Alex Petroski <alexpetroski@gmail.com>
2025-09-30 12:50:51 -0400

    Adding in UPDATE and DELETE mutations

--- client/src/graphql/mutations.ts
+++ client/src/graphql/mutations.ts
@@ -13,3 +13,25 @@ export const CREATE_TRADING_PLAN = gql`
     }
   }
 `;
+
+export const UPDATE_TRADING_PLAN = gql`
+  mutation UpdateTradingPlan($id: ID!, $input: TradingPlanUpdateInput!) {
+    updateTradingPlan(id: $id, input: $input) {
+      id
+      title
+      daily {
+        date
+        marketContext
+      }
+      createdAt
+    }
+  }
+`;
+
+export const DELETE_TRADING_PLAN = gql`
+  mutation DeleteTradingPlan($id: ID!) {
+    deleteTradingPlan(id: $id) {
+      id
+    }
+  }
+`;
